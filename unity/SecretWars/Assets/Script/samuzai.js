2229c9c96b3514f8f10a0904d7fc9760ede70117
isao.shinohara <shinohara@double-rabbits.net>
2014-01-10 01:47:41 +0900

    unity 調整 キャラクター動作

--- unity/SecretWars/Assets/Script/samuzai.js
+++ unity/SecretWars/Assets/Script/samuzai.js
@@ -1,25 +1,46 @@
 ﻿#pragma strict
 
 var speed : float;
+var addSpeed : float;
+var decSpeedCoefficient : float;
+var maxSpeed : float;
 var jumpSpeed : float;
 var gravity : float;
 var rotateSpeed : float;
+var actionIntervalTime : float;
 
 private var moveDirection : Vector3 = Vector3.zero;
 private var controller:CharacterController;
 private var animator : Animator;
+private var state : AnimatorStateInfo;
+private var attackFlag : boolean;
+private var actionStartTime : float;
+private var decSpeedJumpCoefficient : float;
+private var afterAttackSpeed : float;
 
 function Start () {
+	actionStartTime = 0;
+	decSpeedJumpCoefficient = 2;
+	afterAttackSpeed = 0.2;
 	animator = GetComponent(Animator);
-	//animator.SetBool("Attack", true);
-	animator.SetFloat("Speed", 3.1);
+	animator.SetBool("Jump", false);
+	animator.SetFloat("Speed", speed);
 	controller = GetComponent(CharacterController);
 }
 
 function Update () {
+	state = animator.GetCurrentAnimatorStateInfo(0);
+	if(state.IsName("Base Layer.Jump") && !this.animation.isPlaying){
+		animator.SetBool("Jump", false);
+	}
+	if(state.IsName("Base Layer.Attack")){
+		animator.SetBool("Attack", false);
+		attackFlag = true;
+	}
+	
 	CameraAxisControl();
 	jumpControl();
-	//attackControl();
+	attackControl();
 	attachMove();
 	attachRotation();
 }
@@ -27,25 +48,53 @@ function Update () {
 //カメラ軸に沿った移動コントロール
 function  CameraAxisControl(){
 	if (controller.isGrounded) {
-	   var forward : Vector3 = Camera.mainCamera.transform.TransformDirection(Vector3.forward);
-	   var right : Vector3 = Camera.mainCamera.transform.TransformDirection(Vector3.right);
-	   
-	   moveDirection = Input.GetAxis("Horizontal")*right + Input.GetAxis("Vertical")*forward;
-	   moveDirection *= speed;
+		var forward : Vector3 = Camera.mainCamera.transform.TransformDirection(Vector3.forward);
+		var right : Vector3 = Camera.mainCamera.transform.TransformDirection(Vector3.right);
+
+		moveDirection = Input.GetAxis("Horizontal")*right + Input.GetAxis("Vertical")*forward;
+	   	 
+	   	if(!attackFlag){
+			if(moveDirection.magnitude > 0.01){
+				if(speed > maxSpeed){
+					speed = maxSpeed;
+				}else{
+					speed += addSpeed;
+				}
+			}else{
+				if(speed > 0){
+					speed -= (addSpeed * decSpeedCoefficient);
+				}else{
+					speed = 0;
+				}
+			}
+		}else{
+			speed = afterAttackSpeed;
+			attackFlag = false;
+		}
+
+		animator.SetFloat("Speed", speed);
+		moveDirection *= speed;
 	}
 }
 
 //標準的なジャンプコントロール
 function jumpControl (){
-	if (Input.GetButton("Jump")) {
-		//moveDirection.y = jumpSpeed;
+	if (Input.GetButton("Jump") && controller.isGrounded) {
+		animator.SetBool("Jump", true);
+		moveDirection.y = jumpSpeed;
+		speed -= (addSpeed * decSpeedCoefficient / decSpeedJumpCoefficient);
 	}
 }
 
 //攻撃コントロール
 function attackControl (){
-	//if (Input.GetButton("Key Z")){
-	//}
+	if (Input.GetButton("Attack")){
+		if(actionIntervalTime > (Time.realtimeSinceStartup - actionStartTime)){
+			return;
+		}
+		animator.SetBool("Attack", true);
+		actionStartTime = Time.realtimeSinceStartup;
+	}
 }
 
 //移動処理 
