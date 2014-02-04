ec544233120a3bda0c4f5571005c35d206e3781b
isao.shinohara <shinohara@double-rabbits.net>
2014-02-05 00:35:10 +0900

    unity 追加 カメラ移動

---
+++ unity/SecretWars/Assets/Script/MainCamera.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var player : GameObject;
+
+function Start () {
+	var y : float =transform.position.y;
+	var z : float =transform.position.z;
+Debug.Log(y);
+Debug.Log(z);
+}
+
+function Update () {
+	//プレイヤーどの位置に置くか
+	transform.position = Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+}
