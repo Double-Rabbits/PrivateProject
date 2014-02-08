3ebf3c7275ad785135ee6a8cc1d448e4adef8618
isao.shinohara <shinohara@double-rabbits.net>
2014-02-09 04:19:58 +0900

    unity 修正 カメラ 縦方向

--- unity/SecretWars/Assets/Script/MainCamera.js
+++ unity/SecretWars/Assets/Script/MainCamera.js
@@ -3,20 +3,32 @@
 
 var player : GameObject;
 
+var zoomRate : float;
+var smooth : float;
+
+private var wallLeft : GameObject;
+private var wallRight : GameObject;
+private var height : float;
+private var width : float;
 private var mostLeftPosition : float;
 private var mostRightPosition : float;
+private var isZoomed = false;
+private var normalOrthographicSize : float;
+private var zoomOrthographicSize : float;
 
 function Start () {
-	var wallLeft : GameObject = GameObject.Find('wall_left');
-	var wallRight : GameObject = GameObject.Find('wall_right');
-	var height = Camera.main.orthographicSize * 2.0;
-	var width = height * Screen.width / Screen.height;
+	wallLeft = GameObject.Find('wall_left');
+	wallRight = GameObject.Find('wall_right');
+	normalOrthographicSize = Camera.main.orthographicSize;
+	zoomOrthographicSize = normalOrthographicSize * zoomRate;
+	height = normalOrthographicSize * 2.0;
+	width = height * Screen.width / Screen.height;
 	mostLeftPosition = wallLeft.transform.position.x + width/2;
 	mostRightPosition = wallRight.transform.position.x - width/2;
 }
 
 function Update () {
-	// x座標のみプレイヤーに合わせる
+	// x座標
 	var positionX : float = player.transform.position.x;
 	// 画面端調整
 	if(positionX < mostLeftPosition){
@@ -25,13 +37,27 @@ function Update () {
 		positionX = mostRightPosition;
 	}
 	transform.position = Vector3(positionX, transform.position.y, transform.position.z);
+	
+	if(isZoomed == true){
+  		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,zoomOrthographicSize,Time.deltaTime*smooth);
+  		mostLeftPosition = Mathf.Lerp(mostLeftPosition,wallLeft.transform.position.x + width/2,Time.deltaTime*smooth);
+  		mostRightPosition = Mathf.Lerp(mostRightPosition,wallRight.transform.position.x - width/2,Time.deltaTime*smooth);
+     }
+     else{
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,normalOrthographicSize,Time.deltaTime*smooth);
+  		mostLeftPosition = Mathf.Lerp(mostLeftPosition,wallLeft.transform.position.x + width/2,Time.deltaTime*smooth);
+  		mostRightPosition = Mathf.Lerp(mostRightPosition,wallRight.transform.position.x - width/2,Time.deltaTime*smooth);
+     }
 }
 
 function zoomIn(){
-
-	Debug.Log("zoomIn");
+	isZoomed = true;
+	height = zoomOrthographicSize * 2.0;
+	width = height * Screen.width / Screen.height;
 }
 
 function zoomOut(){
-	Debug.Log("zoomOut");
+	isZoomed = false;
+	height = normalOrthographicSize * 2.0;
+	width = height * Screen.width / Screen.height;
 }
--- unity/SecretWars/Assets/Script/SkeletonColliderScript.js
+++ unity/SecretWars/Assets/Script/SkeletonColliderScript.js
@@ -2,19 +2,23 @@
 
 var mainCamera : Camera;
 
+
 private var parentScript : Skeleton;
 private var cameraScript : MainCamera;
+private var runRadius : float;
+private var runRadiusRate : float = 0.6;
 
 function Start () {
 	var  parent : GameObject = this.gameObject.transform.parent.gameObject;
-	//Debug.Log(parent.name);
 	parentScript = parent.GetComponent("Skeleton");
 	cameraScript = mainCamera.GetComponent("MainCamera");
+	var sphereColliderObject : GameObject =  GameObject.Find('SkeletonCollider');
+	var sphereCollider : SphereCollider = sphereColliderObject.GetComponent("SphereCollider");
+	runRadius = sphereCollider.radius * runRadiusRate;
 }
 
 function OnTriggerEnter(other : Collider) {
 	if(other.gameObject.tag == "Player"){
-		//Debug.Log("OnTriggerEnter");
 		cameraScript.SendMessage("zoomIn");
 	}
 }
@@ -22,15 +26,22 @@ function OnTriggerEnter(other : Collider) {
 function OnTriggerStay(other : Collider) {
 	if(!parentScript.damageFlag){
 		if(other.gameObject.tag == "Player"){
-    		parentScript.speed = 1.2;
+			var distance = Vector3.Distance(transform.position, other.transform.position);
+			if(distance < runRadius){
+				parentScript.speed = 1.2;
+			}else{
+				parentScript.speed = parentScript.defaultSpeed;
+			}
 		}
 	}
 }
 
 function OnTriggerExit(other : Collider) {
 	if(other.gameObject.tag == "Player"){
-		//Debug.Log("OnTriggerExit");
-    	parentScript.speed = parentScript.defaultSpeed;
+		var distance = Vector3.Distance(transform.position, other.transform.position);
+		if(distance > runRadius){
+			parentScript.speed = parentScript.defaultSpeed;
+		}
     	cameraScript.SendMessage("zoomOut");
 	}
 }
