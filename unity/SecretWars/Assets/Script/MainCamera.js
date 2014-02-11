cca5f3f2f04ea789d00b50a2f308309ab8d2ceb4
isao.shinohara <shinohara@double-rabbits.net>
2014-02-12 00:45:49 +0900

    unity 変更 カメラ上下

--- unity/SecretWars/Assets/Script/MainCamera.js
+++ unity/SecretWars/Assets/Script/MainCamera.js
@@ -5,6 +5,9 @@ var player : GameObject;
 
 var zoomRate : float;
 var smooth : float;
+var maxCameraPositionVerticalZ : float;
+var minNormalCameraPositionVerticalZ : float;
+var minZoomCameraPositionVerticalZ : float;
 
 private var wallLeft : GameObject;
 private var wallRight : GameObject;
@@ -15,6 +18,9 @@ private var mostRightPosition : float;
 private var isZoomed = false;
 private var normalOrthographicSize : float;
 private var zoomOrthographicSize : float;
+private var initCameraPositionZ : float;
+private var initPlayerPositionZ : float;
+private var minCameraPositionVerticalZ : float;
 
 function Start () {
 	wallLeft = GameObject.Find('wall_left');
@@ -25,6 +31,9 @@ function Start () {
 	width = height * Screen.width / Screen.height;
 	mostLeftPosition = wallLeft.transform.position.x + width/2;
 	mostRightPosition = wallRight.transform.position.x - width/2;
+	initCameraPositionZ = Camera.main.transform.position.z;
+	initPlayerPositionZ = player.transform.position.z;
+	minCameraPositionVerticalZ = minNormalCameraPositionVerticalZ;
 }
 
 function Update () {
@@ -36,17 +45,28 @@ function Update () {
 	}if(positionX > mostRightPosition){
 		positionX = mostRightPosition;
 	}
-	transform.position = Vector3(positionX, transform.position.y, transform.position.z);
+	// y座標
+	var positionZ : float = initCameraPositionZ + player.transform.position.z - initPlayerPositionZ;
+	// 画面端調整
+	if(positionZ < minCameraPositionVerticalZ){
+		positionZ = minCameraPositionVerticalZ;
+	}if(positionZ > maxCameraPositionVerticalZ){
+		positionZ = maxCameraPositionVerticalZ;
+	}
+
+	transform.position = Vector3(positionX, transform.position.y, positionZ);
 	
 	if(isZoomed == true){
   		Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,zoomOrthographicSize,Time.deltaTime*smooth);
   		mostLeftPosition = Mathf.Lerp(mostLeftPosition,wallLeft.transform.position.x + width/2,Time.deltaTime*smooth);
   		mostRightPosition = Mathf.Lerp(mostRightPosition,wallRight.transform.position.x - width/2,Time.deltaTime*smooth);
+  		minCameraPositionVerticalZ = Mathf.Lerp(minCameraPositionVerticalZ,minZoomCameraPositionVerticalZ,Time.deltaTime*smooth);
      }
      else{
         Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize,normalOrthographicSize,Time.deltaTime*smooth);
   		mostLeftPosition = Mathf.Lerp(mostLeftPosition,wallLeft.transform.position.x + width/2,Time.deltaTime*smooth);
   		mostRightPosition = Mathf.Lerp(mostRightPosition,wallRight.transform.position.x - width/2,Time.deltaTime*smooth);
+  		minCameraPositionVerticalZ = Mathf.Lerp(minCameraPositionVerticalZ,minNormalCameraPositionVerticalZ,Time.deltaTime*smooth);
      }
 }
 
